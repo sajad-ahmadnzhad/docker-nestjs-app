2521b3547224bd9a0c57d256323491e0346c2d4d
sajad ahmadnzhad <ahmadnzhad22@gmail.com>
2024-12-17 21:14:51 +0330

    feat: add config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: process.cwd() + '/.env',
+      isGlobal: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const { PORT = 3000 } = process.env
+  const logger = new Logger('NestApplication')
+
+  await app.listen(PORT, () => {
+    logger.log(`Server running on port ${PORT}`)
+  });
 }
 bootstrap();
